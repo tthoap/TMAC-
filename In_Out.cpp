e6164b2f895087c8b9cd99c0470b8867065f597a
hoatran <tthoaptit@gmail.com>
2023-11-15 15:55:49 +0700

    Nov/15 pratice for stro#10

---
+++ In_Out.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    long b;
+    float c;
+    char d;
+    string ho_ten;
+
+    cout << "Hay nhap 1 so nguyen\n";
+    cin >> a;
+    cout << "gia tri cua so nguyen la " << a << endl;
+    cout << "Hay nhap 1 so nguyen dai\n";
+    cin >> b;
+    cout << "gia tri cua so nguyen dai la " << b << endl;
+    cout << "Hay nhap 1 so thuc: \n";
+    cin >> c;
+    cout << "gia tri cua so thuc la " << c << endl;
+    cout << "Hay nhap 1 ki tu\n";
+    cin >> d;
+    cout << "Ky tu la " << d << endl;
+    cout << "Hay nhap Ho ten ban\n";
+    cin >> d;
+    cout << "Ho ten la " << ho_ten << endl;
+    return 0;
+}
